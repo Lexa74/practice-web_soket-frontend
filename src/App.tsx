b39dd0f98777675205ce6e8340f69eaece917dd6
admin <tany--96@mail.ru>
2024-08-10 15:40:53 +0300

    Websocket done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import {useEffect, useState} from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface MessageType {
+    id: string,
+    isViewed: boolean,
+    username: string,
+    message: string
 }
 
-export default App
+export const App = () => {
+    const [messages, setMessages] = useState<MessageType[]>([])
+    const [inputMsg, setInputMsg] = useState("")
+    const [username, setUsername] = useState("")
+    const [ws, setWs] = useState<null | WebSocket>(null)
+
+    useEffect(() => {
+        const socket = new WebSocket("ws://localhost:8080")
+
+        setWs(socket)
+
+        socket.onmessage = (event) => {
+            setMessages(JSON.parse(event.data))
+        }
+
+        socket.onerror = (err) => {
+            console.log("Ошибка", err)
+        }
+
+        socket.onclose = (event) => {
+            console.log("Закрылся поток", event.code, event.reason)
+        }
+    }, []);
+
+    const onChangeMsg = (e) => {
+        setInputMsg(e.target.value)
+    }
+
+    const sendMsg = () => {
+        if(ws && ws.readyState === WebSocket.OPEN) {
+            const body = {
+                username,
+                message: inputMsg,
+                type: "new"
+            }
+            ws.send(JSON.stringify(body))
+            setInputMsg("")
+        } else {
+            console.log("Сокет закрылся")
+        }
+    }
+
+    const onChangeUsername = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const onView = (id: string) => {
+        if(ws && ws.readyState === WebSocket.OPEN) {
+            const body = {
+                id,
+                type: "view"
+            }
+            ws.send(JSON.stringify(body))
+        } else {
+            console.log("Сокет закрылся")
+        }
+    }
+
+    console.log(messages)
+
+    return (
+        <div>
+            <input type="text" onChange={onChangeUsername} value={username} placeholder={"Введите имя"}/>
+            <div className="chat">
+                <div className="message-container">
+                    {messages.map((msg) => (
+                         <div
+                             onClick={() => onView(msg.id)}
+                             key={msg.id}
+                             className={`message-container__message ${msg.username === "Alex" ? "self" : ""} ${msg.isViewed ? "is-viewed" : ""}`}
+                         >
+                             {msg.message}
+                         </div>
+                    ))}
+                </div>
+                <div className="send">
+                    <div onClick={sendMsg}> > </div>
+                    <input onChange={onChangeMsg} value={inputMsg} type="text"/>
+                </div>
+            </div>
+        </div>
+    );
+};
